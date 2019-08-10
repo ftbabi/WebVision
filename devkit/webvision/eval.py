c0db202691e4ef5fc399504dcb33a2fa45315320
Mark <shaoyidi19@163.com>
2019-08-10 20:06:47 +0800

    Modify devkit

---
+++ devkit/webvision/eval.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import config as config
+import pandas as pd
+import numpy as np
+
+
+def MergeVal(info, validation_df):
+    '''
+    Merge with the validation data
+    '''
+    # The first column is always the image_id
+    column = ['image_id', ]
+    column.extend(['predicted_%d' % (k) for k in
+                   range(len(validation_df.columns)-1)])
+    validation_df.columns = column
+    # Sanity check the validation has same number of images
+    assert len(validation_df.image_id.unique()) == \
+        info[info.type == 'val'].shape[0]
+    return info.merge(validation_df, on='image_id')
+
+
+def MergeValFile(info, validation_filename):
+    return MergeVal(info,
+                    config._ParseTextFile(validation_filename))
+
+# Metrics
+def TopKPerClass(df, columns):
+    label_correct = np.min(
+        np.abs((df[columns].sub(
+            df.label, axis='index')).values), axis=1)
+    return float(sum(label_correct == 0)) / len(label_correct)
+
+
+def TopK(df, top_k, class_average, evalset='val'):
+    '''
+    Eval based on the top K accuracy
+    '''
+    top_k_columns = ['predicted_%d' % (k) for k in range(top_k)]
+    # Calculate the top_k accurarcy
+    eval_set = df[df.type == evalset]
+    # Check the set label is not null
+    assert eval_set.label.isnull().sum() == 0, 'Null value exists in label.'
+    if class_average:
+        acc_per_class = []
+        for grp_id, grp in eval_set.groupby('label'):
+            acc = TopKPerClass(grp, top_k_columns)
+            acc_per_class.append(acc)
+            print("Group %s : %f" % (grp_id, acc))
+        return np.mean(acc_per_class)
+    else:
+        return TopKPerClass(eval_set, top_k_columns)
