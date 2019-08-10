6e66674c147db6961ea067dfe1f98f1dff551cc8
Mark <shaoyidi19@163.com>
2019-08-10 20:13:27 +0800

    Modified devkit

--- devkit/webvision/config.py
+++ devkit/webvision/config.py
@@ -88,6 +88,62 @@ def ValidateIntegrity(info):
 
     return True
 
+def LoadTrain():
+    '''
+        Load the dataset info from the dataset
+        '''
+    print("Loading configuration from %s" % (INFO))
+    # Load train file list
+    all_train_data = []
+    for dataset in DATA_SOURCE:
+        # Load training file
+        trn = _ParseTextFile(
+            join(INFO, 'train_filelist_%s.txt' % (dataset)),
+            ['image_id', 'label'])
+        print('Data %s has %d training samples' % (dataset, trn.shape[0]))
+        trn['type'] = 'train'
+        trn['source'] = dataset
+        # Load meta
+        meta = _ParseTextFile(join(INFO, 'train_meta_list_%s.txt' % (dataset)),
+                              ['meta_path', 'row_number'])
+        meta['meta_path'] = meta.meta_path.map(lambda x: join(META_FOLDER, x))
+        all_train_data.append(trn.join(meta[['meta_path']]))
+    training_df = pd.concat(all_train_data)
+    training_df['image_path'] = training_df['image_id'].map(
+        lambda x: join(TRAIN_FOLDER, x))
+
+    data_info = training_df
+    data_info['class_id'] = \
+        data_info['image_id'].map(_ExtractClassName)
+    return data_info
+
+def LoadTest():
+    # Load testing file list
+    test_df = _ParseTextFile(
+        join(INFO, 'test_filelist.txt'),
+        ['image_id'])
+    test_df['type'] = 'test'
+    test_df['image_path'] = test_df['image_id'].map(
+        lambda x: join(TEST_FOLDER, x))
+
+    data_info = test_df
+    data_info['class_id'] = \
+        data_info['image_id'].map(_ExtractClassName)
+    return data_info
+
+def LoadVal():
+    # Load validation file list
+    val_df = _ParseTextFile(
+        join(INFO, 'val_filelist.txt'),
+        ['image_id', 'label'])
+    val_df['type'] = 'val'
+    val_df['image_path'] = val_df['image_id'].map(
+        lambda x: join(VAL_FOLDER, x))
+    data_info = val_df
+    data_info['class_id'] = \
+        data_info['image_id'].map(_ExtractClassName)
+    return data_info
+
 if __name__ == '__main__':
     info = LoadInfo()
     train_set = info.loc[info['type']=='train']
